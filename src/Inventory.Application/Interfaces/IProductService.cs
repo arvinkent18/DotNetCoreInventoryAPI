12b23481a21cc7856dd0c3a1dac887c0b6b25ede
Arvin Kent Lazaga <arvinkent121816@gmail.com>
2024-08-14 23:43:15 +0800

    feat: added reference and initial domain and application layer

---
+++ src/Inventory.Application/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+using Inventory.Domain.Entities;
+
+namespace Inventory.Application.Interfaces
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+    }
+}
---
+++ src/Inventory.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+using Inventory.Domain.Entities;
+
+namespace Inventory.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+    }
+}
