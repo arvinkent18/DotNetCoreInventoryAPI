d0e2e060623c4cee87f7c7cd8bd0d371ba8ae295
Arvin Kent Lazaga <arvinkent121816@gmail.com>
2024-08-15 00:48:43 +0800

    feat: added app DB context

---
+++ src/Inventory.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using Inventory.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Inventory.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            });
+        }
+    }
+}
