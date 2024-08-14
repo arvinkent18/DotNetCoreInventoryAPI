7f6d12aade118ef32e74afab80c3d124d9191ca0
Arvin Kent Lazaga <arvinkent121816@gmail.com>
2024-08-15 00:07:54 +0800

    refactor: handle empty product name field

--- src/Inventory.Domain/Entities/Product.cs
+++ src/Inventory.Domain/Entities/Product.cs
@@ -1,7 +1,19 @@
+
 namespace Inventory.Domain.Entities
 {
     public class Product
     {
+        public Product(int id, string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Product name cannot be empty.", nameof(name));
+            }
+
+            Id = id;
+            Name = name;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
