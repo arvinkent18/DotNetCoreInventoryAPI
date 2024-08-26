fe8ab6c10f596bec8fd0eb8ce61f915aabb18a13
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2024-08-26 23:17:58 +0800

    feat: added global result filter

---
+++ src/Inventory.Web/Filters/ResponseFormatFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Web.Filters;
+    public class ResponseFormatFilter : ResultFilterAttribute
+    {
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        if (context.Result is ObjectResult objectResult)
+        {
+            var originalValue = objectResult.Value;
+            bool isSuccess = objectResult.StatusCode >= 200 && objectResult.StatusCode < 300;
+
+            objectResult.Value = new
+            {
+                success = isSuccess,
+                message = isSuccess ? "Request successful" : "An error occurred",
+                data = isSuccess ? originalValue : null,
+                errors = isSuccess ? null : GetErrorDetails(objectResult)
+            };
+        }
+        base.OnResultExecuting(context);
+    }
+
+    private object GetErrorDetails(ObjectResult objectResult)
+    {
+        return new
+        {
+            error = objectResult.Value?.ToString() ?? "Unknown error"
+        };
+    }
+}
+
