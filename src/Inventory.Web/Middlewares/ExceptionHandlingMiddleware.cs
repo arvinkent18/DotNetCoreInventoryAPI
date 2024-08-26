f77e248c35c4b435d8f934818171d1d076ed2264
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2024-08-26 23:20:12 +0800

    feat: added global exception handling middleware

---
+++ src/Inventory.Web/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Inventory.Application.Exceptions;
+
+namespace Inventory.Web.Middlewares;
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ProductNotFoundException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            context.Response.ContentType = "application/json";
+            var response = new
+            {
+                success = false,
+                message = ex.Message
+            };
+            await context.Response.WriteAsJsonAsync(response);
+        }
+        catch (Exception)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/json";
+            var response = new
+            {
+                success = false,
+                message = "An unexpected error occurred."
+            };
+            await context.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
