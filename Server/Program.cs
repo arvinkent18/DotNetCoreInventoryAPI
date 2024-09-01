3d562f10af94e72a95e411c7e824d354d4a583e4
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2024-09-02 00:45:51 +0800

    feat: initial aspnet db context

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,20 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly.GetName().Name;
 
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly(assembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+       .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
     .AddConfigurationStore(options =>
     {
         options.ConfigureDbContext = b =>
@@ -14,7 +24,7 @@ builder.Services.AddIdentityServer()
         {
             options.ConfigureDbContext = b =>
         b.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"), opt => opt.MigrationsAssembly(assembly));
-    })
+        })
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
